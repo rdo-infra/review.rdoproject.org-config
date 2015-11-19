cc6acb673d73d698cdd0e5f0af639198ce2e7f6f
admin <admin@rpmfactory.beta.rdoproject.org>
2015-11-19 16:04:16 +0000

    JJB/Zuul files

---
+++ nodepool/base.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+set -x
+
+sudo yum update -y
+
+# The jenkins user. Must be able to use sudo without password
+sudo useradd -m jenkins
+sudo gpasswd -a jenkins wheel
+echo "jenkins ALL=(ALL) NOPASSWD:ALL" | sudo tee --append /etc/sudoers.d/90-cloud-init-users
+echo "Defaults   !requiretty" | sudo tee --append /etc/sudoers.d/90-cloud-init-users
+
+# SSH key for the Jenkins user
+sudo mkdir /home/jenkins/.ssh
+sudo cp /opt/nodepool-scripts/authorized_keys /home/jenkins/.ssh/authorized_keys
+sudo ssh-keygen -N '' -f /home/jenkins/.ssh/id_rsa
+sudo chown -R jenkins /home/jenkins/.ssh
+sudo chmod 700 /home/jenkins/.ssh
+sudo chmod 600 /home/jenkins/.ssh/authorized_keys
+sudo restorecon -R -v /home/jenkins/.ssh/authorized_keys
+
+# Nodepool will try to connect on the fresh node using the user
+# defined as username in the provider.image section conf. Usually
+# it is the clouduser. So fetch it and authorize the pub key
+# for that user.
+cloud_user=$(egrep " name:" /etc/cloud/cloud.cfg | awk '{print $2}')
+cat /opt/nodepool-scripts/authorized_keys | sudo tee -a /home/$cloud_user/.ssh/authorized_keys
+
+# Required by Jenkins
+sudo yum install -y java
+
+# zuul-cloner is needed as well
+sudo yum install -y epel-release
+sudo yum install -y python-pip git python-devel gcc patch
+sudo pip install zuul gitdb requests glob2 python-magic argparse python-swiftclient python-keystoneclient
+
+sudo curl -o /usr/local/bin/zuul_swift_upload.py \
+    https://raw.githubusercontent.com/openstack-infra/project-config/master/jenkins/scripts/zuul_swift_upload.py
+sudo chmod +x /usr/local/bin/zuul_swift_upload.py
+
+# sync FS, otherwise there are 0-byte sized files from the yum/pip installations
+sudo sync
+
+sudo cat /home/jenkins/.ssh/authorized_keys
+sudo cat /home/centos/.ssh/authorized_keys
+
+echo "Base setup done."
---
+++ nodepool/sf_slave_setup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+. base.sh
+
+# This should be removed after we fix functestlib.sh and how
+# we manage to store artifacts
+sudo useradd www-data
+
+sudo yum install -y epel-release
+
+sudo yum install -y git python-augeas bridge-utils curl lxc wget swig python-devel python-pip graphviz python-yaml openssl-devel libffi-devel pigz mysql-devel openldap-devel qemu-img libvirt-daemon-lxc git-review
+sudo pip install flake8 bash8 ansible
+sudo pip install -U tox==1.6.1 Sphinx oslosphinx virtualenv restructuredtext_lint python-swiftclient
+
+sudo dd if=/dev/zero of=/srv/swap count=4000 bs=1M
+sudo chmod 600 /srv/swap
+sudo mkswap /srv/swap
+grep swap /etc/fstab || echo "/srv/swap none swap sw 0 0" | sudo tee -a /etc/fstab
+
+sudo sed -i 's/^.*SELINUX=.*$/SELINUX=disabled/' /etc/selinux/config
+
+sudo mkdir -p /var/lib/sf
+sudo mkdir -p /var/lib/sf/artifacts/logs
+sudo chown -R jenkins:jenkins /var/lib/sf/
+
+# Temporary DNS fix
+echo "216.58.213.16 gerrit-releases.storage.googleapis.com" | sudo tee -a /etc/hosts
+
+# sync FS, otherwise there are 0-byte sized files from the yum/pip installations
+sudo sync
+
+echo "Setup finished. Creating snapshot now, this will take a few minutes"
