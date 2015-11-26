462e15d3030ba2124dbac088984ff9c5fbed27dc
Sebastien Badia <sbadia@redhat.com>
2015-11-26 18:16:47 +0100

    nodepool: Add koji nodepool scripts (insecure certs for now)
    Change-Id: I98f48269a8d51be03c7b0f6246af7eea0825d8e0

---
+++ nodepool/koji_slave_setup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+. base.sh
+
+# This should be removed after we fix functestlib.sh and how
+# we manage to store artifacts
+sudo useradd www-data
+
+sudo yum install -y fedora-packager wget python-pip
+sudo pip install -U tox==1.6.1 python-swiftclient
+
+sudo dd if=/dev/zero of=/srv/swap count=4000 bs=1M
+sudo chmod 600 /srv/swap
+sudo mkswap /srv/swap
+grep swap /etc/fstab || echo "/srv/swap none swap sw 0 0" | sudo tee -a /etc/fstab
+
+sudo sed -i 's/^.*SELINUX=.*$/SELINUX=disabled/' /etc/selinux/config
+
+sudo mkdir -p /var/lib/sf
+sudo mkdir -p /var/lib/sf/artifacts/logs
+sudo chown -R jenkins:jenkins /var/lib/sf/
+
+# Koji configuration and certs (insecure)
+cat /opt/nodepool-scripts/koji/koji.conf | sudo tee /etc/koji.conf
+sudo tar xvzf /opt/nodepool-scripts/koji/jenkins-koji.tgz -C /home/jenkins/
+sudo chown -R jenkins /home/jenkins/.koji
+
+# Test koji communication
+sudo -u jenkins 'koji' 'moshimoshi'
+
+# sync FS, otherwise there are 0-byte sized files from the yum/pip installations
+sudo sync
+
+echo "Setup finished. Creating snapshot now, this will take a few minutes"
