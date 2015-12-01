99e0ae41497f2a9b94f0367f41ccfbfac5e8c5d8
Sebastien Badia <sbadia@redhat.com>
2015-12-01 16:32:30 +0100

    nodepool: Clean koji setup script and re-enable SELinux
    Change-Id: I4fc511b5c5e1ded34cfe80fbfd44f707447adc85

--- nodepool/koji_slave_setup.sh
+++ nodepool/koji_slave_setup.sh
@@ -2,23 +2,13 @@
 
 . base.sh
 
-# This should be removed after we fix functestlib.sh and how
-# we manage to store artifacts
-sudo useradd www-data
-
-sudo yum install -y fedora-packager wget python-pip
-sudo pip install -U tox==1.6.1 python-swiftclient
+sudo yum install -y koji wget python-pip
+sudo pip install -U tox==1.6.1
 
 sudo dd if=/dev/zero of=/srv/swap count=4000 bs=1M
 sudo chmod 600 /srv/swap
 sudo mkswap /srv/swap
 grep swap /etc/fstab || echo "/srv/swap none swap sw 0 0" | sudo tee -a /etc/fstab
-
-sudo sed -i 's/^.*SELINUX=.*$/SELINUX=disabled/' /etc/selinux/config
-
-sudo mkdir -p /var/lib/sf
-sudo mkdir -p /var/lib/sf/artifacts/logs
-sudo chown -R jenkins:jenkins /var/lib/sf/
 
 # Koji configuration and certs (insecure)
 sudo wget http://192.168.42.27/conf/koji.conf -O /etc/koji.conf
@@ -26,13 +16,9 @@ sudo wget http://192.168.42.27/conf/jenkins-koji.tgz -O /srv/jenkins-koji.tgz
 sudo tar xvzf /srv/jenkins-koji.tgz -C /home/jenkins/
 sudo chown -R jenkins /home/jenkins/.koji
 
-# Test koji communication
-sudo -u jenkins 'koji' 'moshimoshi'
-
 # install ansible
-
 sudo yum install -y epel-release
-sudo yum install -y python-pip python-crypto git
+sudo yum install -y python-crypto git
 sudo pip install -U ansible==1.9.2
 ansible --version
 sudo yum remove -y epel-release
