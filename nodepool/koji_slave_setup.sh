fb9ef096652215f274acaf92c7fb868f429fed75
Sebastien Badia <sbadia@redhat.com>
2015-12-04 19:36:10 +0100

    nodepool/koji_slave_setup.sh: be sure to install python-crypto from package
    Change-Id: Iaa2b85b834dcae953328b695cdfc371f53665275

--- nodepool/koji_slave_setup.sh
+++ nodepool/koji_slave_setup.sh
@@ -2,12 +2,20 @@
 
 . base.sh
 
+# install ansible
+sudo yum install -y epel-release
+sudo yum install -y python-crypto git python-pip
+sudo pip install -U ansible==1.9.2
+ansible --version
+sudo yum remove -y epel-release
+
 # install tox and gcc/python-dev to be able to build native python
 # modules
 sudo yum install -y koji wget rpmdevtools rpm-build redhat-rpm-config
-sudo yum install -y python-pip git python-devel gcc patch libffi-devel
+sudo yum install -y python-devel gcc patch libffi-devel
 sudo pip install -U tox==1.6.1
 
+# swap
 sudo dd if=/dev/zero of=/srv/swap count=4000 bs=1M
 sudo chmod 600 /srv/swap
 sudo mkswap /srv/swap
@@ -18,12 +26,6 @@ sudo wget http://192.168.42.27/conf/koji.conf -O /etc/koji.conf
 sudo wget http://192.168.42.27/conf/jenkins-koji.tgz -O /srv/jenkins-koji.tgz
 sudo tar xvzf /srv/jenkins-koji.tgz -C /home/jenkins/
 sudo chown -R jenkins /home/jenkins/.koji
-
-# install ansible
-sudo yum install -y epel-release python-crypto
-sudo pip install -U ansible==1.9.2
-ansible --version
-sudo yum remove -y epel-release
 
 # install khaleesi
 git clone https://github.com/redhat-openstack/khaleesi.git
