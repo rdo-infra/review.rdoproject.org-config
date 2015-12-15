96257cb9334fd7ee41b7ba036ab52132c25f2b44
Sebastien Badia <sbadia@redhat.com>
2015-12-15 20:33:22 +0100

    Setup slaves as thick OpenStack Infra slaves
    See:
      - http://softwarefactory-project.io/r/#/c/2229/
    
    Change-Id: I3dd6adf6c825090cbad0fbd54d897d69870275a8

--- nodepool/koji_slave_setup.sh
+++ nodepool/koji_slave_setup.sh
@@ -7,7 +7,6 @@ sudo yum install -y epel-release
 sudo yum install -y python-crypto git python-pip
 sudo pip install -U ansible==1.9.2
 ansible --version
-sudo yum remove -y epel-release
 
 # install tox and gcc/python-dev to be able to build native python
 # modules
@@ -27,6 +26,18 @@ sudo wget http://192.168.42.27/conf/jenkins-koji.tgz -O /srv/jenkins-koji.tgz
 sudo tar xvzf /srv/jenkins-koji.tgz -C /home/jenkins/
 sudo chown -R jenkins /home/jenkins/.koji
 
+# OpenStack projects configuration (tox+deps)
+# http://docs.openstack.org/developer/requirements/
+git clone http://softwarefactory-project.io/r/rpmfactory
+sudo cp rpmfactory/slaves/cloud.repo /etc/yum.repos.d/
+sudo cp rpmfactory/slaves/run-tox.sh /usr/local/bin/
+
+# https://github.com/openstack-infra/system-config/blob/master/modules/openstack_project/manifests/thick_slave.pp
+sudo yum install -y mariadb-devel postgresql-devel libjpeg-devel
+sudo yum install -y gcc-c++ zeromq-devel libxslt-devel sqlite-devel
+sudo yum install -y libvirt-devel openldap-devel libxml2-devel
+sudo yum install -y liberasurecode-devel
+
 # install khaleesi
 git clone https://github.com/redhat-openstack/khaleesi.git
 git clone https://github.com/redhat-openstack/khaleesi-settings.git
@@ -37,5 +48,9 @@ popd
 
 # sync FS, otherwise there are 0-byte sized files from the yum/pip installations
 sudo sync
+
+# Remove epel and cloud repo
+sudo yum remove -y epel-release
+sudo rm /etc/yum.repos.d/cloud.repo
 
 echo "Setup finished. Creating snapshot now, this will take a few minutes"
