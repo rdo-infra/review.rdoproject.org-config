8367cbfdbd8850e1e05a2fab2daf325b16c654a5
Sebastien Badia <sbadia@redhat.com>
2016-01-14 10:32:38 +0100

    nodepool: Add mock package (for delorean tests)
    Change-Id: I64b38a111f0a861aa3dafabb4b02128acb07d585

--- nodepool/koji_slave_setup.sh
+++ nodepool/koji_slave_setup.sh
@@ -11,7 +11,7 @@ ansible --version
 # install tox and gcc/python-dev to be able to build native python
 # modules
 sudo yum install -y koji wget rpmdevtools rpm-build redhat-rpm-config
-sudo yum install -y python-devel gcc patch libffi-devel
+sudo yum install -y python-devel gcc patch libffi-devel mock
 sudo pip install -U tox==1.6.1
 
 # swap
