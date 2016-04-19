d0661333c77aeaca01f3b3081f6d357b0f057e59
David Moreau-Simard <dms@redhat.com>
2016-04-19 11:08:05 -0400

    Optimize bare-centos-7 and rdo-centos7-liberty images
    - Remove usage of EPEL (setuptools -> easy_install -> pip -> tox)
    - Don't install things like khaleesi and ansible by default
    - Remove duplicated install of packages
    - Use centos-release-openstack-liberty package instead of
      setting up repository ourselves
    
    It was necessary to "fork" base.sh and rpmfactory-base.sh as
    they are part of software factory and any changes to them would
    be overwritten by an update.
    base.sh was forked mostly to get rid of setup using EPEL.
    The only reason for rdo-rpmfactory-base.sh to exist is because
    the default one imports base.sh which we want to replace by
    rdo-base.sh.
    
    Change-Id: I0f69f47dc033bf6a6af1ebedf71cf6b89d5607bf

---
+++ nodepool/rdo-base.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+# RDO "fork" of base.sh
+
+set -xe
+
+sudo yum update -y > /dev/null
+
+# Base requirements
+sudo yum install -y python-setuptools python-virtualenv git wget curl patch \
+                    iproute libffi-devel
+sudo easy_install pip
+sudo pip install tox
+
+# The jenkins user. Must be able to use sudo without password
+sudo useradd -m jenkins
+sudo gpasswd -a jenkins wheel
+echo "jenkins ALL=(ALL) NOPASSWD:ALL" | sudo tee --append /etc/sudoers.d/90-cloud-init-users
+echo "Defaults   !requiretty" | sudo tee --append /etc/sudoers.d/90-cloud-init-users
+
+# SSH key for the Jenkins user
+sudo mkdir /home/jenkins/.ssh
+sudo cp /opt/nodepool-scripts/authorized_keys /home/jenkins/.ssh/authorized_keys
+sudo ssh-keygen -N '' -f /home/jenkins/.ssh/id_rsa
+sudo chown -R jenkins /home/jenkins/.ssh
+sudo chmod 700 /home/jenkins/.ssh
+sudo chmod 600 /home/jenkins/.ssh/authorized_keys
+sudo restorecon -R -v /home/jenkins/.ssh/authorized_keys
+
+# Nodepool will try to connect on the fresh node using the user
+# defined as username in the provider.image section conf. Usually
+# it is the clouduser. So fetch it and authorize the pub key
+# for that user.
+cloud_user=$(egrep " name:" /etc/cloud/cloud.cfg | awk '{print $2}')
+cat /opt/nodepool-scripts/authorized_keys | sudo tee -a /home/$cloud_user/.ssh/authorized_keys
+
+# Install java (required by Jenkins)
+sudo yum install -y java
+
+# Install zuul_swift_upload and zuul-cloner
+# TODO: replace this section by zuul package
+sudo yum install -y python-requests gcc python-devel
+sudo pip install zuul glob2 python-magic
+
+# Copy slave tools
+sudo cp -v /opt/nodepool-scripts/*.py /usr/local/bin/
+
+# sync FS, otherwise there are 0-byte sized files from the yum/pip installations
+sudo sync
+
+echo "Base setup done."
