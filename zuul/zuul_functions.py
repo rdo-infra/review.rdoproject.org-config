e5ad3ff0f2bcfddd4400e0131f126893687af3cf
David Moreau-Simard <dms@redhat.com>
2016-07-12 14:46:01 -0400

    Revert "Override Zuul's LOG_PATH to use ZUUL_REF as unique identifier"
    This commit reverts ea4bb1dc81f617d92b773ad487ef6a01334a44cc and
    55b39b0b4ada0b4c315996e58e7331ef965d1c0d.
    
    Change-Id: I5a590b4e0cbbc2be8193b95e3b47ae312298f057

--- zuul/zuul_functions.py
+++ zuul/zuul_functions.py
@@ -13,15 +13,3 @@ def set_node_options(item, job, params):
         return
     print "zuul_functions: set_node_options(", item, job, "): "
     params['OFFLINE_NODE_WHEN_COMPLETE'] = '1'
-
-    # Override Zuul's LOG_PATH to use ZUUL_REF as unique identifier
-    # instead of ZUUL_UUID. ZUUL_REF is unique across a job build set
-    # while ZUUL_UUID is unique per job. Uploading based on ZUUL_REF
-    # allows child jobs to find parent jobs artifacts easily.
-    ref_uuid = params['ZUUL_REF'].split('/')[-1]
-    params['LOG_PATH'] = "{base}/{pipeline}/{job}/{ref}".format(
-        base=params['BASE_LOG_PATH'],
-        pipeline=params['ZUUL_PIPELINE'],
-        job=job.name,
-        ref=ref_uuid
-    )
