ced3f721ece96669d7f7e3d8a51bc47353b21cb5
David Moreau-Simard <dms@redhat.com>
2016-08-17 13:13:17 -0400

    Pre-cache cirros files for use with weirdo packstack and puppet jobs
    Change-Id: I9f309a7e88feeaa7cb62a583cee627ef70ca524a

--- nodepool/rdo-weirdo.sh
+++ nodepool/rdo-weirdo.sh
@@ -6,3 +6,9 @@
 
 # See "rdo-weirdo-get-packages.sh" for package retrieval method
 cat weirdo-packages.txt |xargs sudo yum -y install
+
+# Pre-cache Cirros images for use with puppet-openstack-integration and
+# Packstack tests
+mkdir -p ~/cache/files
+wget --tries=10 http://download.cirros-cloud.net/0.3.4/cirros-0.3.4-x86_64-uec.tar.gz -P ~/cache/files/
+wget --tries=10 http://download.cirros-cloud.net/0.3.4/cirros-0.3.4-x86_64-disk.img -P ~/cache/files/
