451e163d863fee4e84a9e23b59afc5526146bce8
Javier Pena <jpena@redhat.com>
2016-10-13 18:15:48 +0200

    Create job to test rdoinfo updates with DLRN
    This job will check all the updated branches for packages in
    rdoinfo, and build them with DLRN.
    
    Change-Id: I6a3e89aec06c30218a81ed075b56166f61f2a870

---
+++ jobs/scripts/rdoinfo-DLRN.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+set -ex
+
+# Simple script to test several DLRN packages 
+GIT_BASE_URL="https://review.rdoproject.org/r/p"
+TAG="${1:-ocata-uc}"
+PACKAGES_TO_BUILD="${2:-python-glanceclient}"
+PROJECT_DISTRO_BRANCH="rpm-master"
+
+# Setup virtualenv with tox and use it
+tox -epy27 --notest
+. .tox/py27/bin/activate
+
+# Prepare config
+target="centos"
+baseurl="http://trunk.rdoproject.org/centos7/"
+src="master"
+branch=""
+tag="ocata-uc"
+
+# If we're testing a commit on a specific branch, make sure we're using it
+if [[ "${TAG}" != "ocata-uc" ]]; then
+    branch=$(echo $TAG | awk -F- '{print $1}')
+    tag=$TAG
+    baseurl="http://trunk.rdoproject.org/${branch}/centos7/"
+    src="stable/${branch}"
+    if [ $TAG = "newton" ]; then
+        PROJECT_DISTRO_BRANCH='newton-rdo'
+    else
+        PROJECT_DISTRO_BRANCH="rpm-${TAG}"
+    fi
+fi
+
+# Update the configuration
+sed -i "s%target=.*%target=${target}%" projects.ini
+sed -i "s%source=.*%source=${src}%" projects.ini
+sed -i "s%baseurl=.*%baseurl=${baseurl}%" projects.ini
+sed -i "s%tags=.*%tags=${tag}%" projects.ini
+
+PACKAGE_LINE=""
+# Prepare directories
+mkdir -p data/repos
+for PACKAGE in ${PACKAGES_TO_BUILD}; do
+    PROJECT_TO_BUILD_MAPPED=$(./scripts/map-project-name $PACKAGE ../rdoinfo)
+    PROJECT_DISTRO=$(./scripts/map-distgit-name $PACKAGE ../rdoinfo)
+    PROJECT_DISTRO_DIR=${PROJECT_TO_BUILD_MAPPED}_distro
+
+    if [ -e /usr/bin/zuul-cloner ]; then
+        zuul-cloner --workspace data/ $GIT_BASE_URL $PROJECT_DISTRO --branch $PROJECT_DISTRO_BRANCH
+        mv data/$PROJECT_DISTRO data/$PROJECT_DISTRO_DIR
+    else
+        # We're outside the gate, just do a regular git clone
+        pushd data/
+        # rm -rf first for idempotency
+        rm -rf $PROJECT_DISTRO_DIR
+        git clone "${GIT_BASE_URL}/${PROJECT_DISTRO}" $PROJECT_DISTRO_DIR
+        cd $PROJECT_DISTRO_DIR
+        git checkout $PROJECT_DISTRO_BRANCH
+        popd
+    fi
+    PACKAGE_LINE="$PACKAGE_LINE --package-name $PACKAGE"
+done
+
+
+# If the commands below throws an error we still want the logs
+function copy_logs() {
+    mkdir -p logs
+    rsync -avzr data/repos logs/centos
+}
+trap copy_logs ERR EXIT
+
+# Run DLRN
+dlrn --config-file projects.ini --head-only $PACKAGE_LINE --dev --info-repo ../rdoinfo
+copy_logs
+# Clean up mock cache, just in case there is a change for the next run
+mock -r data/dlrn-1.cfg --scrub=all
