8d37f76044b3f7128367a6601ee2058daa84de6f
David Moreau-Simard <dms@redhat.com>
2017-06-20 18:05:48 -0400

    Build upstream openstack-infra centos-7 DIB image on review.rdoproject.org
    This is a first attempt at building the upstream centos-7 image in the
    nodepool of review.rdoproject.org.
    Prior to this, a synchronization of the openstack-infra/project-config
    repository has been set up on the nodepool-builder instance with the help
    of the "project-config-nodepool.sh" also part of this commit.
    
    Change-Id: I7cdf09aca313a0bcc525a8e5087bf2a6f9ae0e8b

---
+++ project-config-nodepool.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+# Retrieves or updates an existing repository for
+# git.openstack.org/openstack-infra/project-config for the purpose of re-using
+# the nodepool elements and scripts from it.
+
+# This is set up as a cron on nodepool-builder instances as such:
+# */5 * * * * /usr/local/bin/project-config-nodepool.sh 2>&1 | systemd-cat -t project-config-nodepool.sh -p info
+# Logs available with "journalctl -t project-config-nodepool.sh"
+
+LOCKFILE="/tmp/project-config-nodepool.lock"
+SERVER="git.openstack.org"
+DIR="/opt/git"
+NAMESPACE="openstack-infra"
+PROJECT="project-config"
+PROJECT_DIR="${DIR}/${NAMESPACE}/${PROJECT}"
+
+# Check for lockfile so we don't pile up processes (i.e, if git is stuck)
+if [[ -e "${LOCKFILE}" ]]; then
+    pid=$(cat "${LOCKFILE}")
+    echo "${0} already running as pid ${pid}, killing it"
+    kill "${pid}"
+    exit
+fi
+
+# Make sure the lockfile is removed when we exit and then claim it
+trap "rm -f ${LOCKFILE}; exit" INT TERM EXIT
+echo $$ > ${LOCKFILE}
+
+if [[ ! -d "${DIR}/${NAMESPACE}" ]]; then
+    echo "${DIR}/${NAMESPACE} doesn't exist, creating it..."
+    mkdir -p "${DIR}/${NAMESPACE}"
+fi
+if [[ ! -d "${PROJECT_DIR}" ]]; then
+    echo "${PROJECT_DIR} doesn't exist, cloning it..."
+    git clone "git://${SERVER}/${NAMESPACE}/${PROJECT}" "${PROJECT_DIR}"
+else
+    echo "${PROJECT_DIR} exists, updating it..."
+    pushd ${PROJECT_DIR}
+        git reset --hard
+        git pull origin master
+    popd
+fi
+
+sleep 75
+
+echo "Setting up symbolic links..."
+ln -sf ${PROJECT_DIR}/nodepool/elements /etc/nodepool/elements/project-config
+ln -sf ${PROJECT_DIR}/nodepool/scripts /etc/nodepool/scripts/project-config
+echo "Done."
+
+rm -f "${LOCKFILE}"
