1d1b5055547b68c1a3f7528033693e03ad67d20c
Sandro Bonazzola <sbonazzo@redhat.com>
2017-08-01 07:48:38 +0200

    Add rubygem-coveralls to review.rdoproject.org
    Change-Id: Iee254fc352fb41a9dc1ef589603442cc4c8e0ba8

--- sanity-check.py
+++ sanity-check.py
@@ -46,6 +46,7 @@ IGNORED_PROJECTS = {
         'centos-opstools/osops-tools-monitoring-oschecks',
         'centos-opstools/rubygem-atomic',
         'centos-opstools/rubygem-cool.io',
+        'centos-opstools/rubygem-coveralls',
         'centos-opstools/rubygem-diff-lcs',
         'centos-opstools/rubygem-domain_name',
         'centos-opstools/rubygem-fluent-plugin-parser',
