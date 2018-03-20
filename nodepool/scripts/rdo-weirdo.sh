186b5f81a57ab0e350f73a3ef7e8a83acd238b1e
Michael Turek <mjturek@linux.vnet.ibm.com>
2018-03-20 19:23:04 +0000

    weirdo: Add logic for choosing proper cirros image based on arch
    This patch adds logic to the nodepool/rdo-weirdo.sh script to
    cache the correct image based on the architecture of the node.
    
    Change-Id: I8fc9af853d0072449966297936cfaca6f47431d6

--- nodepool/scripts/rdo-weirdo.sh
+++ nodepool/scripts/rdo-weirdo.sh
@@ -13,8 +13,20 @@ cat weirdo-packages.txt |xargs sudo yum -y install
 
 sudo sed -i 's/^SELINUX=.*/SELINUX=permissive/g' /etc/sysconfig/selinux
 
+arch="$(uname -p)"
+
 # Pre-cache Cirros images for use with puppet-openstack-integration and
 # Packstack tests
 mkdir -p ~/cache/files
-wget --tries=10 http://download.cirros-cloud.net/0.3.4/cirros-0.3.4-x86_64-uec.tar.gz -P ~/cache/files/
-wget --tries=10 http://download.cirros-cloud.net/0.3.4/cirros-0.3.4-x86_64-disk.img -P ~/cache/files/
+
+case $arch in
+    x86_64)
+        cirros_version="0.3.4"
+        ;;
+    ppc64le)
+        cirros_version="0.4.0"
+        ;;
+esac
+
+wget --tries=10 http://download.cirros-cloud.net/$cirros_version/cirros-$cirros_version-$arch-uec.tar.gz -P ~/cache/files/
+wget --tries=10 http://download.cirros-cloud.net/$cirros_version/cirros-$cirros_version-$arch-disk.img -P ~/cache/files/
