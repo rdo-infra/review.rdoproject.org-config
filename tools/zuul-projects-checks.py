45054910fe6578f4fce9a3b21bf441acceb43dbd
Paul Belanger <pabelanger@redhat.com>
2018-06-21 19:46:44 -0400

    Add tox-linters jobs for config project
    This allows us to start check jobs on changes to zuul.d yaml files. It
    will help ensure users configure the correct templates for projects.
    
    Change-Id: If0de73d33b85549a76b7ef50b4c859bc84678529
    Signed-off-by: Paul Belanger <pabelanger@redhat.com>

---
+++ tools/zuul-projects-checks.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+
+# Copyright 2017 SUSE Linux GmbH
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import sys
+
+import yaml
+
+
+projects_yaml = 'zuul.d/projects.yaml'
+projects = yaml.safe_load(open(projects_yaml))
+
+
+def check_system_templates():
+    """Check that each repo has a system-required template."""
+
+    errors = False
+    print("\nChecking for usage of system-required")
+    print("=====================================")
+    for entry in projects:
+        project = entry['project']
+        # TODO(mordred) Generalize this, but for now, avoid doing this check
+        # on ansible.
+        if project['name'] == 'ansible/ansible':
+            continue
+        try:
+            correct = False
+            for template in project['templates']:
+                if template == 'system-required':
+                    correct = True
+            if not correct:
+                raise
+        except:
+            print("  ERROR: Project %s has no system-required template" %
+                  project['name'])
+            errors = True
+
+    if not errors:
+        print("... all fine.")
+    return errors
+
+
+def check_all():
+
+    errors = check_system_templates()
+
+    if errors:
+        print("\nFound errors in zuul.d/projects.yaml!\n")
+    else:
+        print("\nNo errors found in zuul.d/projects.yaml!\n")
+    return errors
+
+
+if __name__ == "__main__":
+    sys.exit(check_all())
