13a4fa32bf25bd359d0ef2ace7a7e350f43691cd
Javier Pena <jpena@redhat.com>
2018-06-26 19:41:52 +0200

    Fix add-project-from-rdoinfo for Zuul v3
    We need to change the python script to create the right lines for
    Zuul v3, instead of the previous Zuul v2 configuration.
    
    Since the process now requires creating three different reviews
    (2 on review.rdo and one on softwarefactory-project.io), this commit
    creates the two reviews in review.rdo, and informs that the third
    review is required.
    
    Note that this script will sort zuul.d/projects.yaml using the project
    name as a key, so if projects are added but unsorted, an automated
    review will move them around. Also, it tries to preserve the existing
    indentation, but a first review will change it, since it is not 100%
    consistent.
    
    Change-Id: If2030b4df575e9cac4b1a303295cce8344d1279c
    TG-1687

--- playbooks/legacy-rdoinfo-create-project/files/add-project-from-rdoinfo.sh
+++ playbooks/legacy-rdoinfo-create-project/files/add-project-from-rdoinfo.sh
@@ -32,10 +32,12 @@ git config user.email "javier.pena@redhat.com"
 git config gitreview.username "rdo-trunk"
 git checkout -b new-project-$PROJECT_NAME
 git review -s -v
-COMMIT_MSG="Create project for $PROJECT_NAME\n\nThis is an automatically created commit, make sure you check the Zuul\nconfiguration to see if it matches the project needs."
+COMMIT_MSG="Create project for $PROJECT_NAME\n\nThis is an automatically created commit, make sure you check the Zuul\nconfiguration to see if it matches the project needs.\n\nNote that a an additional review is required on [1].\n\n[1] - http://softwarefactory-project.io/r/gitweb?p=config.git;a=blob;f=zuul/rdo.yaml"
 git add resources/*.yaml
 git add gerritbot/*.yaml
-git add zuul/*.yaml
 echo -e $COMMIT_MSG | git commit -F-
-git review -t "add-${PROJECT_NAME}" < /dev/null
+git add zuul.d/*.yaml
+COMMIT_MSG="Add Zuul definitions for rally-openstack $PROJECT_NAME\n\nNote that this review will fail to pass CI until the corresponding entries \nhave been created in the softwarefactory-project.io config repo."
+echo -e $COMMIT_MSG | git commit -F-
+git review -y -t "add-${PROJECT_NAME}" < /dev/null
 popd
