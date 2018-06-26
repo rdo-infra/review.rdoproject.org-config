a3be9a6116de79e872c72bbe6d05badb67fae0df
Javier Pena <jpena@redhat.com>
2018-06-26 19:41:52 +0200

    [WIP] Fix add-project-from-rdoinfo for Zuul v3
    We need to change the python script to create the right lines for
    Zuul v3, instead of the previous Zuul v2 configuration.
    
    Still WIP, need to ensure the right blanks are added.
    
    Change-Id: If2030b4df575e9cac4b1a303295cce8344d1279c

--- playbooks/legacy-rdoinfo-create-project/files/add-project-from-rdoinfo.py
+++ playbooks/legacy-rdoinfo-create-project/files/add-project-from-rdoinfo.py
@@ -40,50 +40,43 @@ def add_project_resources(prefix, name, maintainers):
                                 indent=4, block_seq_indent=2))
 
     # And also to the Zuul configuration
-    with open("zuul/projects.yaml") as infile:
+    with open("zuul.d/projects.yaml") as infile:
         info = yaml.load(infile, Loader=yaml.RoundTripLoader)
 
-    data = yaml.comments.CommentedMap([('name', "%s/%s-distgit" %
-                                        (project_prefix, project_name)),
-                                       ('template',
-                                        [yaml.comments.CommentedMap(
-                                            [('name',
-                                              'package-distgit-check-jobs')])])
-                                       ])
-    data['template'][0].ca.items['name'] = [None, None,
-                                            yaml.tokens.CommentToken(
-                                                '\n\n',
-                                                yaml.error.CommentMark(0),
-                                                None), None]
-    if data not in info['projects']:
-        info['projects'].append(data)
+    data = yaml.comments.CommentedMap(
+        [('project', yaml.comments.CommentedMap(
+            [('name', "%s/%s-distgit" % (project_prefix, project_name)),
+             ('templates', yaml.comments.CommentedSeq(['package-distgit-check-jobs', 'system-required']))])
+        )])
+
+
+    data['project']['templates'].ca.items[1] = [
+         yaml.tokens.CommentToken('\n\n', yaml.error.CommentMark(0), None), None, None, None]
+
+    if data not in info:
+        info.append(data)
     else:
         print("Zuul config already includes %s/%s-distgit" %
               (project_prefix, project_name))
 
-    data = yaml.comments.CommentedMap([('name', "%s/%s" %
-                                        (project_prefix, project_name)),
-                                       ('template',
-                                        [yaml.comments.CommentedMap(
-                                            [('name', 'package-check-jobs')])])
-                                       ])
+    data = yaml.comments.CommentedMap(
+        [('project', yaml.comments.CommentedMap(
+            [('name', "%s/%s-distgit" % (project_prefix, project_name)),
+             ('templates', yaml.comments.CommentedSeq(['package-check-jobs', 'system-required']))])
+        )])
 
-    data['template'][0].ca.items['name'] = [None, None,
-                                            yaml.tokens.CommentToken(
-                                                '\n\n',
-                                                yaml.error.CommentMark(0),
-                                                None), None]
-    if data not in info['projects']:
-        info['projects'].append(data)
+    data['project']['templates'].ca.items[1] = [
+         yaml.tokens.CommentToken('\n\n', yaml.error.CommentMark(0), None), None, None, None]
+
+    if data not in info:
+        info.append(data)
     else:
         print("Zuul config already includes %s/%s" %
               (project_prefix, project_name))
 
-    info['projects'].sort()
-
-    with open('zuul/projects.yaml', 'w') as outfile:
+    with open('zuul.d/projects.yaml', 'w') as outfile:
         outfile.write(yaml.dump(info, Dumper=yaml.RoundTripDumper,
-                                indent=4, block_seq_indent=2))
+                                indent=2))
 
 if __name__ == '__main__':
     add_project_resources(sys.argv[1], sys.argv[2], sys.argv[3])
