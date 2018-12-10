f4a060c6daf96df9d04530fec09cd17168043d0f
Rafael Folco <rfolco@redhat.com>
2018-12-10 17:48:37 -0200

    Add f28 promotion job containers-build
    With the premise of failing at the beginning, the job
    **periodic-tripleo-fedora-28-master-containers-build**
    depends on promote-consistent-to-tripleo-ci-testing job to run.
    
    kolla-build settings that are being changed:
     * kolla_base
     * kolla_base_image
     * kolla_base_tag
     * kolla_virtualenv
     * kolla_push
     * trunk_repository
     * trunk_deps_repository
    
    Manually tested this change to make sure it does not break
    centos7 containers build process. See:
    https://tree.taiga.io/project/tripleo-ci-board/task/567
    
    The job won't build any container until f28 support merges
    in kolla. When this happen it will be not pushing anything
    to rdo trunk registry, so it is a safe move.
    
    Story: https://tree.taiga.io/project/tripleo-ci-board/us/447
    
    Change-Id: I53682b45db9b1c4b38afc55e8ba677892c4dbfff

--- ci-scripts/tripleo-upstream/build-containers-images.sh
+++ ci-scripts/tripleo-upstream/build-containers-images.sh
@@ -23,6 +23,12 @@ else
     CPU_ARCH="-$CPU_ARCH"
 fi
 
+DISTRO="${DISTRO_NAME}${DISTRO_VERSION}"
+
+if [ "$DISTRO_NAME" = "fedora" ]; then
+    PUSH="false"
+fi
+
 cat << EOF > $WORKSPACE/playbook.yml
 ---
 - name: Build Kolla images
@@ -30,11 +36,16 @@ cat << EOF > $WORKSPACE/playbook.yml
   become: yes
   become_user: root
   vars:
+    kolla_base: "${DISTRO_NAME}"
+    kolla_base_image: "${DISTRO_NAME}"
+    kolla_base_tag: "${DISTRO_VERSION}"
+    kolla_virtualenv: "{{ kolla_tmpdir }}/.tox/build-${DISTRO_NAME}-binary"
     kolla_namespace: "tripleo${RELEASE}${CPU_ARCH}"
-    kolla_push: true
+    kolla_push: "${PUSH:-true}"
     kolla_tag: "$TESTING_TAG"
     openstack_release: "$RELEASE"
-    trunk_repository: "https://trunk.rdoproject.org/centos7-$RELEASE/$TESTING_TAG/delorean.repo"
+    trunk_repository: "https://trunk.rdoproject.org/${DISTRO}-$RELEASE/$TESTING_TAG/delorean.repo"
+    trunk_deps_repository: "https://trunk.rdoproject.org/${DISTRO}-$RELEASE/delorean-deps.repo"
   tasks:
     - include_role:
         name: "rdo-kolla-build"
