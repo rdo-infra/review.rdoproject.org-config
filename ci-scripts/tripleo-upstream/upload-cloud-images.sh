abd9b3cfa6155c0ba959d3bc2125b3897933534a
Rafael Folco <rfolco@redhat.com>
2018-12-10 18:05:16 -0200

    Add pure f28 promotion jobs (bm/containers) to periodic pipeline
    This change depends on [1], [3], and [4] to merge:
    
    The stucture of the "pure" f28 promotion pipeline is:
    
    [] (centos7/f28) promote-consistent-to-tripleo-ci-testing* [5]
     \
      [] f28 containers-build (parent commit [1])
       \
        [] f28 featureset002-upload [2]
        [] f28 standalone [3]
    
    Related changes:
     - https://review.rdoproject.org/r/17738 # config: safe to merge [1]
     - https://review.rdoproject.org/r/17807 # rdo-jobs: merged [2]
     - https://review.rdoproject.org/r/17744 # config: merged [5]
    
    *[5] shared centos7/f28 promotion job
    
    Note: it was agreed that the pure fedora-28 jobs would exist in the
    openstack-periodic pipeline alongside the centos-7 jobs.
    
    Depends-On: https://review.rdoproject.org/r/17739 # rdo-jobs [3]
    Depends-On: https://review.rdoproject.org/r/17886 # rdo-infra-playbooks [4]
    
    Change-Id: I9f4132f6d7d4a389465a2b7612b7fade7edef2ae

--- ci-scripts/tripleo-upstream/upload-cloud-images.sh
+++ ci-scripts/tripleo-upstream/upload-cloud-images.sh
@@ -8,7 +8,7 @@ ls *.tar
 
 export RSYNC_RSH="ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
 rsync_cmd="rsync --verbose --archive --delay-updates --relative"
-UPLOAD_URL=uploader@images.rdoproject.org:/var/www/html/images/$RELEASE/rdo_trunk
+UPLOAD_URL=uploader@images.rdoproject.org:/var/www/html/images/$DISTRO/$RELEASE/rdo_trunk
 # Check if directory $FULL_HASH exists, if not create it.
 if [ ! -d $FULL_HASH ]; then
     mkdir $FULL_HASH
