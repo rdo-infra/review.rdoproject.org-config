386e07c9b54151f0deae71d7f5e13fd58fe2687c
Rafael Folco <rfolco@redhat.com>
2019-11-28 11:50:09 -0300

    New job to promote component to current-tripleo
    In the integration promotion this is done in the promoter code.
    For component we just need to promote the repo link when the
    component criteria jobs successed.
    
    This change also switches dlrnapi server for the component jobs.
    
    https://tree.taiga.io/project/tripleo-ci-board/task/1401
    
    Change-Id: I9a1edef8000a51077486415de2832fb67b685a1a
    Task: https://tree.taiga.io/project/tripleo-ci-board/task/1401

---
+++ ci-scripts/tripleo-upstream/dlrnapi_check_reports.sh
@@ -0,0 +1,10 @@
+source $WORKSPACE/hash_info.sh
+pip_cmd=$(command -v pip || command -v pip3)
+$pip_cmd install --user dlrnapi-client shyaml
+PATH=$PATH:/home/$USER/.local/bin
+dlrnapi --url $DLRNAPI_URL \
+    repo-status  \
+    --commit-hash $COMMIT_HASH \
+    --distro-hash $DISTRO_HASH \
+    --success true \
+    | grep $1 \
