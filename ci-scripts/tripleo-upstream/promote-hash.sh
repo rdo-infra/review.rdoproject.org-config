532f4319558101b4e670b5f85dc01d970bc0944c
Ronelle Landy <rlandy@redhat.com>
2019-12-16 20:54:37 -0500

    Add options to replace defaults for downstream usage
    roles/scripts that are used downstream are required to use
    different default values.
    This review adds the variables which can have overrides
    downstream.
    
    Change-Id: Iec05ff1e6e6ec0f3a5308c0ece74ab30d0038cc3

--- ci-scripts/tripleo-upstream/promote-hash.sh
+++ ci-scripts/tripleo-upstream/promote-hash.sh
@@ -9,12 +9,13 @@ trap deactivate_dlrnapi_venv EXIT
 activate_dlrnapi_venv
 
 source $WORKSPACE/hash_info.sh
+: ${DLRNAPI_USERNAME:="review_rdoproject_org"}
 
 set -u
 
 # Assign label to the specific hash using the DLRN API
 dlrnapi --url $DLRNAPI_URL \
-    --username review_rdoproject_org \
+    --username $DLRNAPI_USERNAME \
     repo-promote \
     --commit-hash $COMMIT_HASH \
     --distro-hash $DISTRO_HASH \
