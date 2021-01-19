eae098b8ece1b5173b3ea90b79bc589ae351c86e
Ronelle Landy <rlandy@redhat.com>
2021-01-19 15:39:26 -0500

    Include --extended-hash option for dlrn promotion
    Downstream hashes now contain an extended_hash.
    When working downstream, include the extended_hash
    when running the promote script.
    
    Depends-On: https://review.rdoproject.org/r/#/c/31602/
    Change-Id: I50a91a841c4bfbb8a24eea0014fbe37c6255c0ab
    Related-Bug: BZ1917198

--- ci-scripts/tripleo-upstream/dlrnapi_report.sh
+++ ci-scripts/tripleo-upstream/dlrnapi_report.sh
@@ -7,6 +7,10 @@ fi
 
 if [[ "$FULL_HASH" == *"_"* ]]; then
     HASH_ARGS=" --commit-hash $COMMIT_HASH --distro-hash $DISTRO_HASH"
+    # When voting on downstream include the extended hash
+    # if  [[ -v $EXTENDED_HASH ]]; then
+    #    HASH_ARGS="$HASH_ARGS --extended_hash $EXTENDED_HASH"
+    # fi
 else
     HASH_ARGS="--agg-hash $FULL_HASH"
 fi
