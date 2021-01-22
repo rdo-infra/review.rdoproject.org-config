95692f9f9cf754fa14eb3e8dae04ea4aeca1883c
Ronelle Landy <rlandy@redhat.com>
2021-01-22 15:30:56 -0500

    Add $EXTENDED_HASH to rest of dlrn-relate scripts
    Change-Id: I0fa8da802668f182d80b50d6b96c1cf234afa53d

--- ci-scripts/tripleo-upstream/check-commit-hash-promote-target.sh
+++ ci-scripts/tripleo-upstream/check-commit-hash-promote-target.sh
@@ -3,15 +3,23 @@
 set -eu
 
 : ${PROMOTE_TARGET:="promoted-components"}
+: ${EXTENDED_HASH:=""}
 
 source $WORKSPACE/hash_info.sh
 pip_cmd=$(command -v pip || command -v pip3)
 $pip_cmd install --user dlrnapi-client shyaml
 PATH=$PATH:/home/$USER/.local/bin
 
+DLRN_API_HASH_ARGS="--commit-hash $COMMIT_HASH \
+    --distro-hash $DISTRO_HASH"
+
+if [[ ! -z $EXTENDED_HASH ]]; then
+    DLRN_API_HASH_ARGS="$DLRN_API_HASH_ARGS \
+    --extended-hash $EXTENDED_HASH"
+fi
+
 dlrnapi --url $DLRNAPI_URL \
     promotion-get  \
-    --commit-hash $COMMIT_HASH \
-    --distro-hash $DISTRO_HASH \
+    $DLRN_API_HASH_ARGS \
     --promote-name $PROMOTE_TARGET \
     | tee -a $WORKSPACE/commit_hash_promote_target_output.txt
--- ci-scripts/tripleo-upstream/dlrnapi_check_reports.sh
+++ ci-scripts/tripleo-upstream/dlrnapi_check_reports.sh
@@ -2,12 +2,22 @@
 
 set -eu
 
+: ${EXTENDED_HASH:=""}
+
 source $WORKSPACE/hash_info.sh
 pip_cmd=$(command -v pip || command -v pip3)
 $pip_cmd install --user dlrnapi-client shyaml
 PATH=$PATH:/home/$USER/.local/bin
+
+DLRN_API_HASH_ARGS="--commit-hash $COMMIT_HASH \
+    --distro-hash $DISTRO_HASH"
+
+if [[ ! -z $EXTENDED_HASH ]]; then
+    DLRN_API_HASH_ARGS="$DLRN_API_HASH_ARGS \
+    --extended-hash $EXTENDED_HASH"
+fi
+
 dlrnapi --url $DLRNAPI_URL \
     repo-status  \
-    --commit-hash $COMMIT_HASH \
-    --distro-hash $DISTRO_HASH \
+    $DLRN_API_HASH_ARGS \
     --success true | tee -a $WORKSPACE/repo_success_output.txt
