23494b7e6c18c66bd45e2d0ffae462c54a0aaab9
Javier Pena <jpena@redhat.com>
2021-04-27 17:52:35 +0200

    Add automation for new RDO dependencies
    We want to automate the process of adding new RDO dependency
    repositories after they are included in rdoinfo. This commit modifies
    the rdoinfo-create-project to add support for dependencies as well
    as normal RDO Trunk distgits.
    
    Change-Id: I57cabe69328a44d3ef508cad1176cf29890c114f

--- playbooks/rdoinfo/create-project/files/add-project-from-rdoinfo.py
+++ playbooks/rdoinfo/create-project/files/add-project-from-rdoinfo.py
@@ -22,11 +22,12 @@ def add_project_resources(prefix, name, maintainers):
     with open("gerritbot/channels.yaml") as gerritbot:
         info = yaml.load(gerritbot, Loader=yaml.RoundTripLoader)
 
-    newkey = "%s/%s-distgit" % (project_prefix, project_name)
-    if newkey not in info['rdo']['projects']:
-        info['rdo']['projects'].append(newkey)
-    else:
-        print("Key %s already in gerritbot" % newkey)
+    if prefix != 'deps':
+        newkey = "%s/%s-distgit" % (project_prefix, project_name)
+        if newkey not in info['rdo']['projects']:
+            info['rdo']['projects'].append(newkey)
+        else:
+            print("Key %s already in gerritbot" % newkey)
 
     newkey = "%s/%s" % (project_prefix, project_name)
     if newkey not in info['rdo']['projects']:
@@ -39,62 +40,64 @@ def add_project_resources(prefix, name, maintainers):
         outfile.write(yaml.dump(info, Dumper=yaml.RoundTripDumper,
                                 indent=4, block_seq_indent=2))
 
-    # And also to the Zuul configuration
+    # And also to the Zuul configuration, but only for distgits
+    # The specific -distgit repo is only created for RDO Trunk packages,
+    # not deps
+    if prefix != 'deps':
+        yml = yaml.YAML()
+        yml.indent(mapping=2, sequence=4, offset=2)
 
-    yml = yaml.YAML()
-    yml.indent(mapping=2, sequence=4, offset=2)
+        with open("zuul.d/projects.yaml") as infile:
+            info = yml.load(infile)
 
-    with open("zuul.d/projects.yaml") as infile:
-        info = yml.load(infile)
+        data = yaml.comments.CommentedMap(
+            [('project', yaml.comments.CommentedMap(
+                [('name', "review.rdoproject.org/%s/%s-distgit" % (project_prefix, project_name)),
+                 ('default-branch', 'rpm-master'),
+                 ('templates', yaml.comments.CommentedSeq(['package-distgit-check-jobs', 'system-required']))])
+            )])
 
-    data = yaml.comments.CommentedMap(
-        [('project', yaml.comments.CommentedMap(
-            [('name', "review.rdoproject.org/%s/%s-distgit" % (project_prefix, project_name)),
-             ('default-branch', 'rpm-master'),
-             ('templates', yaml.comments.CommentedSeq(['package-distgit-check-jobs', 'system-required']))])
-        )])
+        data['project']['templates'].ca.items[1] = [
+             yaml.tokens.CommentToken('\n\n', yaml.error.CommentMark(0), None), None, None, None]
 
-    data['project']['templates'].ca.items[1] = [
-         yaml.tokens.CommentToken('\n\n', yaml.error.CommentMark(0), None), None, None, None]
+        if data not in info:
+            info.append(data)
+        else:
+            print("Zuul config already includes %s/%s-distgit" %
+                  (project_prefix, project_name))
 
-    if data not in info:
-        info.append(data)
-    else:
-        print("Zuul config already includes %s/%s-distgit" %
-              (project_prefix, project_name))
+        data = yaml.comments.CommentedMap(
+            [('project', yaml.comments.CommentedMap(
+                [('name', "review.rdoproject.org/%s/%s" % (project_prefix, project_name)),
+                 ('templates', yaml.comments.CommentedSeq(['package-check-jobs', 'system-required']))])
+            )])
 
-    data = yaml.comments.CommentedMap(
-        [('project', yaml.comments.CommentedMap(
-            [('name', "review.rdoproject.org/%s/%s" % (project_prefix, project_name)),
-             ('templates', yaml.comments.CommentedSeq(['package-check-jobs', 'system-required']))])
-        )])
+        data['project']['templates'].ca.items[1] = [
+             yaml.tokens.CommentToken('\n\n', yaml.error.CommentMark(0), None), None, None, None]
 
-    data['project']['templates'].ca.items[1] = [
-         yaml.tokens.CommentToken('\n\n', yaml.error.CommentMark(0), None), None, None, None]
+        if data not in info:
+            info.append(data)
+        else:
+            print("Zuul config already includes %s/%s" %
+                  (project_prefix, project_name))
 
-    if data not in info:
-        info.append(data)
-    else:
-        print("Zuul config already includes %s/%s" %
-              (project_prefix, project_name))
+        info = sorted(info, key=lambda i: i['project']['name'])
+        yml.dump(info, open('zuul.d/projects.yaml', 'w'))
 
-    info = sorted(info, key=lambda i: i['project']['name'])
-    yml.dump(info, open('zuul.d/projects.yaml', 'w'))
-
-    # Strip the extra 2 spaces that ruamel.yaml appends because we told it
-    # to indent an extra 2 spaces. Because the top level entry is a list it
-    # applies that indentation at the top. It doesn't indent the comment lines
-    # extra though, so don't do them.
-    with open('zuul.d/projects.yaml', 'r') as fp:
-        content = fp.readlines()
-    with open('zuul.d/projects.yaml', 'w') as fp:
-        for line in content:
-            if '#' in line:
-                fp.write(line)
-            elif len(line) < 2:
-                fp.write(line)
-            else:
-                fp.write(line[2:])
+        # Strip the extra 2 spaces that ruamel.yaml appends because we told it
+        # to indent an extra 2 spaces. Because the top level entry is a list it
+        # applies that indentation at the top. It doesn't indent the comment lines
+        # extra though, so don't do them.
+        with open('zuul.d/projects.yaml', 'r') as fp:
+            content = fp.readlines()
+        with open('zuul.d/projects.yaml', 'w') as fp:
+            for line in content:
+                if '#' in line:
+                    fp.write(line)
+                elif len(line) < 2:
+                    fp.write(line)
+                else:
+                    fp.write(line[2:])
 
 
 def add_project_package(prefix, name):
@@ -103,13 +106,23 @@ def add_project_package(prefix, name):
     with open('resources/rdo.yaml') as fp:
         resource =  yaml.load(fp, Loader=yaml.RoundTripLoader, preserve_quotes=True)
 
-    project = "%s/%s-distgit" % (prefix, name)
-    data = yaml.comments.CommentedMap(
-        [(project, yaml.comments.CommentedMap([
-            ('zuul/include', []),
-            ('repoxplorer/branches', ["rpm-master"]),
-            ('default-branch', 'rpm-master'),
-        ]))])
+    if prefix != 'deps':
+        project = "%s/%s-distgit" % (prefix, name)
+        data = yaml.comments.CommentedMap(
+            [(project, yaml.comments.CommentedMap([
+                ('zuul/include', []),
+                ('repoxplorer/branches', ["rpm-master"]),
+                ('default-branch', 'rpm-master'),
+            ]))])
+    else:
+        project = "%s/%s" % (prefix, name)
+        data = yaml.comments.CommentedMap(
+            [(project, yaml.comments.CommentedMap([
+                ('zuul/include', []),
+                ('repoxplorer/branches', ["c9s-xena-rdo"]),
+                ('default-branch', 'c9s-xena-rdo'),
+            ]))])
+
     if data not in resource['resources']['projects']['RDO']['source-repositories']:
         resource['resources']['projects']['RDO']['source-repositories'].append(data)
     else:
@@ -123,6 +136,7 @@ def add_project_package(prefix, name):
                            Dumper=yaml.RoundTripDumper,
                            indent=2,
                            block_seq_indent=0))
+
 
 if __name__ == '__main__':
     add_project_resources(sys.argv[1], sys.argv[2], sys.argv[3])
