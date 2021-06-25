d6d41c10bdf23195af546220104a58b366445853
Joel Capitao <jcapitao@redhat.com>
2021-06-25 17:32:39 +0200

    DNM Patch to test locally review 33518
    The commands to run:
    pushd playbooks/rdo-deps-uc-send-review
    ./setup-env-test.sh
    ansible-playbook run.yaml -vvv
    
    Change-Id: I24eed8e9685743b7ecb8cb8cb96fce70af4140c8

---
+++ playbooks/rdo-deps-uc-send-review/setup-env-test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+rm -rf workspace
+mkdir workspace
+
+pushd workspace
+
+git clone git@github.com:rdo-infra/releng.git
+git clone https://github.com/openstack/requirements
+git clone https://review.rdoproject.org/cgit/gating_scripts
+pushd gating_scripts
+git review -d 33506
+popd
+git clone https://github.com/redhat-openstack/rdoinfo
+pushd rdoinfo
+echo "- project: python-aniso8601" | tee -a buildsys-tags/cloud8s-openstack-xena-candidate.yml >/dev/null
+echo "  buildsys-tags:" | tee -a buildsys-tags/cloud8s-openstack-xena-candidate.yml >/dev/null
+echo "    cloud8s-openstack-xena-candidate: python-aniso8601-8.1.1-1.el8" | tee -a buildsys-tags/cloud8s-openstack-xena-candidate.yml >/dev/null
+popd
+
+popd
