34f0fc27dacfc66e9ee5b4f5ada3bec9cc916d7b
Joel Capitao <jcapitao@redhat.com>
2022-07-08 11:11:28 +0200

    Rewrite checking opened patches as a function
    To reduce the basecode and ease understanding, we move
    huge chunk into function.
    
    Change-Id: Ic9d38e7651a15f6d01ffabf2913a0b44342af6a9

---
+++ playbooks/rdo-deps-uc-send-review/files/functions.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# function is_patch_already() is used to get URL of patches
+# which bump the dependency to desired version.
+# params: pkg_name, pkg_vers and branch
+# returns: 0 if URLs found, 1 else
+#          it appends the DEPENDS_ON variable with the URLs
+function is_patch_already() {
+    local pkg_name=${1:-$pkg_name}
+    local pkg_vers=${2:-$pkg_vers}
+    local branch=${3:-$branch}
+	local change_endpoint_url=""
+	local change_url=""
+	local specfile=""
+
+    local opened_changes=$(ssh -p 29418 amoralej+rdo-trunk@review.rdoproject.org \
+                           gerrit query --format=JSON \
+                           status:open \
+                           project:deps/${pkg_name} \
+                           branch:${branch}
+                          )
+    local change_ids=$(echo "$opened_changes" | jq '.id' | tr -d '"' | sed "s/null//")
+
+    if [ -n "$change_ids" ]; then
+        echo -e "INFO: there are opened patches for ${pkg_name}"
+    else
+        echo -e "INFO: there are not opened patches for ${pkg_name}"
+    fi
+
+    for change_id in $change_ids; do
+        change_endpoint_url="https://review.rdoproject.org/r/changes/deps%2F$pkg_name~$branch~$change_id/revisions/current"
+        specfile=$(curl -s -f $change_endpoint_url/files/ | sed 1d | jq 'keys[]' | tr -d '"' | grep -e "\.spec$")
+        if [ $? -eq 0 ]; then
+            specfile=$(echo "$specfile" | sed "s/\//%2f/")
+            curl -s -f $change_endpoint_url/files/$specfile/content | base64 -d | grep -e "^Version:.*$pkg_vers" >/dev/null 2>&1
+            if [ $? -eq 0 ]; then
+                change_url=$(echo "$opened_changes" | jq --arg CHANGE_ID "$change_id" '. | select(.id==$CHANGE_ID) | .url' | tr -d '"')
+                echo -e "INFO: adding depends-on $change_url patch"
+                DEPENDS_ON+="Depends-On: $change_url\n"
+                return 0
+            fi
+        fi
+    done
+    echo -e "INFO: there are not opened patches for ${pkg_name} matching the required ${pkg_vers}"
+	return 1
+}
