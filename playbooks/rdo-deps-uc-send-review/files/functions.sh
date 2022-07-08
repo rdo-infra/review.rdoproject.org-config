73e9fd867235d0003c863a5555111cd201d57deb
Joel Capitao <jcapitao@redhat.com>
2022-07-08 15:28:39 +0200

    Get change list from report-uc periodically
    Currently, we only run the job when a patch is merged
    in upstream requirements u-c file [1].
    Now, we want the job to also check periodically if builds
    are available for the modules and required version.
    
    The steps below:
      - Periodically, we get the packages which are not synced
        with required vers (with report-uc.py python script)
      - Then for each package:
          - we check whether the NVR is already proposed in
            rdoinfo patch or not, if so we move to next
            package.
          - then, we check if NVR is in Fedora Koji, if it's not
            available then we move to next package.
          - then, we check if NVR is in CBS Koji, if so
            we add the NVR in rdoinfo final patch, else
            we submit a distgit patch.
      - we submit the rdoinfo final patch
    
    [1] https://github.com/openstack/requirements/blob/master/upper-constraints.txt
    
    Change-Id: I68e818bb7f4591949b736e1d1750cd1b1fc765ac

--- playbooks/rdo-deps-uc-send-review/files/functions.sh
+++ playbooks/rdo-deps-uc-send-review/files/functions.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-# function is_patch_already() is used to get URL of patches
-# which bump the dependency to desired version.
+# function is_patch_already() aims to check whether a patch
+# which bumps the dependency to desired version exists or not.
 # params: pkg_name, pkg_vers and branch
-# returns: 0 if URLs found, 1 else
-#          it appends the DEPENDS_ON variable with the URLs
+# returns: 0 if patch found, 1 else
 function is_patch_already() {
     local pkg_name=${1:-$pkg_name}
     local pkg_vers=${2:-$pkg_vers}
@@ -21,12 +20,6 @@ function is_patch_already() {
                           )
     local change_ids=$(echo "$opened_changes" | jq '.id' | tr -d '"' | sed "s/null//")
 
-    if [ -n "$change_ids" ]; then
-        echo -e "INFO: there are opened patches for ${pkg_name}"
-    else
-        echo -e "INFO: there are not opened patches for ${pkg_name}"
-    fi
-
     for change_id in $change_ids; do
         change_endpoint_url="https://review.rdoproject.org/r/changes/deps%2F$pkg_name~$branch~$change_id/revisions/current"
         specfile=$(curl -s -f $change_endpoint_url/files/ | sed 1d | jq 'keys[]' | tr -d '"' | grep -e "\.spec$")
@@ -34,13 +27,48 @@ function is_patch_already() {
             specfile=$(echo "$specfile" | sed "s/\//%2f/")
             curl -s -f $change_endpoint_url/files/$specfile/content | base64 -d | grep -e "^Version:.*$pkg_vers" >/dev/null 2>&1
             if [ $? -eq 0 ]; then
-                change_url=$(echo "$opened_changes" | jq --arg CHANGE_ID "$change_id" '. | select(.id==$CHANGE_ID) | .url' | tr -d '"')
-                echo -e "INFO: adding depends-on $change_url patch"
-                DEPENDS_ON+="Depends-On: $change_url\n"
                 return 0
             fi
         fi
     done
-    echo -e "INFO: there are not opened patches for ${pkg_name} matching the required ${pkg_vers}"
 	return 1
+}
+
+# function is_in_rdoinfo_patch_already() aims to check if
+# the NVR is already present in rdoinfo opened patch.
+# params: nvr, release and topic
+# returns: 0 if NVR found, 1 else
+function is_in_rdoinfo_patch_already() {
+    local nvr=${1:-$nvr}
+    local release=${2:-$release}
+    local topic=${3:-$topic}
+    local tmp_dir="/tmp/rdoinfo_${topic}_opened_change_ids/"
+    local cache_filename="${tmp_dir}/ids"
+	mkdir $tmp_dir >/dev/null 2>&1
+	# We cache the files for 10 minutes
+	is_cache=$(find $cache_filename -type f -mmin -10 2>/dev/null)
+	if [ ! -f "$is_cache" ]; then
+        ssh -p 29418 amoralej+rdo-trunk@review.rdoproject.org \
+                     gerrit query --format=JSON \
+                     status:open \
+                     project:rdoinfo \
+                     topic:$topic \
+                     branch:master | jq '.id' | tr -d '"' | sed "s/null//"> $cache_filename
+
+        while read change_id; do
+            change_endpoint_url="https://review.rdoproject.org/r/changes/rdoinfo~master~$change_id/revisions/current"
+            specfile=$(curl -s -f $change_endpoint_url/files/ | sed 1d | jq 'keys[]' | tr -d '"' | grep -e "${release}-testing.yml$")
+            if [ $? -eq 0 ]; then
+                specfile=$(echo "$specfile" | sed "s/\//%2f/")
+                curl -s -f $change_endpoint_url/files/$specfile/content | base64 -d > ${tmp_dir}/${change_id}-${release}-testing.yml
+            fi
+        done < $cache_filename
+    fi
+
+    grep -e "$nvr" $tmp_dir/* >/dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        return 0
+    else
+        return 1
+    fi
 }
