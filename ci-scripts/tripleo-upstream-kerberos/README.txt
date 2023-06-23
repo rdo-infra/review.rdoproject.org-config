49be2a31d35df4d292bb5f9c49af6938a35551ee
Enrique Vallespi Gil <evallesp@redhat.com>
2023-06-23 13:27:51 +0200

    Add testing kerberos roles for promote-hash role and tripleo-upstream ci-scripts.
    In following patches it would be fine to refactor them in order to install kerberos version for dlrnapi and use it or not depending some environments variable.
    At the moment is a first testing version.
    
    Change-Id: I32be1cf9b08a9b35f175c9f927ece411716c8270

---
+++ ci-scripts/tripleo-upstream-kerberos/README.txt
@@ -0,0 +1,17 @@
+The following files are here because to zuul secrets
+encycpted passwords and keys are required for the
+proper operation of these scripts.
+
+convert-upload-undercloud.sh
+build-containers-images.sh
+upload-cloud-images.sh
+dlrnapi_check_reports.sh
+dlrnapi_report.sh
+promote-hash.sh
+
+Please only add a script here if a
+secret is required. It is not possible
+to check a change prior to merge
+with scripts here.
+
+These scripts are added to start authenticate by kerberos.
---
+++ ci-scripts/tripleo-upstream-kerberos/upload-cloud-images.sh
@@ -0,0 +1,57 @@
+set -e
+echo ======== UPLOAD CLOUD IMAGES
+source $WORKSPACE/hash_info.sh
+
+pushd $HOME
+
+ls *.tar *.qcow2 || true
+
+export RSYNC_RSH="ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
+rsync_cmd="rsync --verbose --archive --no-perms --no-owner --no-group --delay-updates --relative"
+
+DISTRO="${DISTRO_NAME}${DISTRO_VERSION}"
+
+# If set by downstream/internal SF jobs don't override UPLOAD_URL
+if [ -z "$UPLOAD_URL" ]; then
+    if [ -n "$DISTRO" ]; then
+        if [[ "$DISTRO" =~ "redhat" ]]; then
+            UPLOAD_URL=rcm-uploader@images.rdoproject.org:/var/www/html/images/rhel/images/$DISTRO/$RELEASE/rdo_trunk
+        else
+            UPLOAD_URL=uploader@images.rdoproject.org:/var/www/html/images/$DISTRO/$RELEASE/rdo_trunk
+        fi
+    else
+        # Legacy url for very old ditro-unaware jobs
+        UPLOAD_URL=uploader@images.rdoproject.org:/var/www/html/images/$RELEASE/rdo_trunk
+    fi
+fi
+echo "UPLOAD_URL is $UPLOAD_URL"
+
+# Note(chandankumar): downgrade rysnc for CentOS Stream 8
+# due to https://bugs.launchpad.net/tripleo/+bug/1984035
+if [[ "$DISTRO" =~ "centos8" ]]; then
+    sudo dnf -y downgrade rsync-3.1.3-14.el8
+fi
+
+# Check if directory $FULL_HASH exists, if not create it.
+if [ ! -d $FULL_HASH ]; then
+    mkdir -p $FULL_HASH
+fi
+if [ -f "overcloud-full.tar" ]; then
+    mv overcloud-full.tar overcloud-full.tar.md5 $FULL_HASH
+fi
+if [ -f "overcloud-hardened-uefi-full.qcow2" ]; then
+    mv overcloud-hardened-uefi-full.qcow2 overcloud-hardened-uefi-full.qcow2.md5 $FULL_HASH
+fi
+if [ -f "ironic-python-agent.tar" ]; then
+    mv ironic-python-agent.tar ironic-python-agent.tar.md5 $FULL_HASH
+fi
+
+$rsync_cmd $FULL_HASH $UPLOAD_URL
+$rsync_cmd --delete --include 'tripleo-ci-testing**' --exclude '*' ./ $UPLOAD_URL/
+# Creating link to tripleo-ci-testing with actual $FULL_HASH
+ln -s $FULL_HASH tripleo-ci-testing
+
+rsync -av tripleo-ci-testing $UPLOAD_URL
+
+popd
+echo ======== UPLOAD CLOUD IMAGES COMPLETE
