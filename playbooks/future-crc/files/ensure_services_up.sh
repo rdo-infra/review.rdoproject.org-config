41471f4e136b2a7ebaad212f8478a1312b4657f8
Daniel Pawlik <dpawlik@redhat.com>
2024-04-09 16:43:19 +0200

    Add base-future-extracted-crc to check machineconfigpool
    The process to avoid machineconfigpool degradation is complicated and it
    requires to perform all changes before host is rebooted, such as:
    
    - inject pull secret into /var/lib/kubelet/config.json
    - changing /etc/node-sizing.env or /etc/node-sizing-enabled.env
    
    It requires also to remove the elements from the machineconfigpool "dump"
    in the missing manifest.
    
    Change-Id: I2d8bd3f6803629a0854de09c8be5998113f5d72b

---
+++ playbooks/future-crc/files/ensure_services_up.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+for i in {1..60}; do
+    echo "Ensuring that containers are spawned... ${i}"
+    not_running_pods=$(/usr/bin/oc get pods --no-headers --all-namespaces | grep -viE 'running|complete')
+    count=$(echo -n "$not_running_pods" | wc -l)
+    if [ "$count" -eq "0" ]; then
+        echo "Cluster seems to be running"
+        exit 0
+    else
+        echo -e "Some core CRC pods are not ready...\n$not_running_pods"
+        sleep 10
+    fi
+done
+
+echo -e "\nSomthing is not deployed in CRC. Exit!\n"
+exit 1
