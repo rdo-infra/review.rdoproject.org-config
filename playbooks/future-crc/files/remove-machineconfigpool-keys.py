84ceab6b194a1d484906d968cac4ab3314f1b436
Daniel Pawlik <dpawlik@redhat.com>
2024-04-09 16:43:19 +0200

    Add base-future-extracted-crc to check machineconfigpool
    The process to avoid machineconfigpool degradation is complicated and it
    requires to perform all changes before host is rebooted, such as:
    
    - inject pull secret into /var/lib/kubelet/config.json
    - changing /etc/node-sizing.env or /etc/node-sizing-enabled.env
    
    It requires also to remove the elements from the machineconfigpool "dump"
    in the missing manifest.
    
    Change-Id: I2d8bd3f6803629a0854de09c8be5998113f5d72b

---
+++ playbooks/future-crc/files/remove-machineconfigpool-keys.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+import yaml
+import os
+import sys
+
+# Usage: remove-keys.py machineconfigpool/render-master.yaml new-master.yaml
+
+content_to_remove = ['/var/lib/kubelet/config.json',
+                     '/etc/node-sizing-enabled.env']
+
+
+def read_yaml(yaml_path):
+    with open(yaml_path) as f:
+        return yaml.safe_load(f)
+
+
+def write_yaml(yaml_path, yaml_content):
+    with open(yaml_path, 'w') as f:
+        yaml.safe_dump(yaml_content, f)
+
+
+def remove_keys(yaml_content):
+    new_files = []
+    if yaml_content.get('spec').get('config').get('storage').get('files'):
+        for file in yaml_content['spec']['config']['storage']['files']:
+            if 'path' in file and file['path'] not in content_to_remove:
+                new_files.append(file)
+        yaml_content['spec']['config']['storage']['files'] = new_files
+        return yaml_content
+    else:
+        print("File does not contain valid machineconfigpool manifest")
+        os.exit(1)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Please provide more arguments, like:")
+        print("remove-keys.py machineconfigpool/master.yaml new-master.yaml")
+        os.exit(1)
+
+    yaml_path = sys.argv[1]
+    yaml_content = read_yaml(yaml_path)
+    changed_yaml = remove_keys(yaml_content)
+    write_yaml(sys.argv[2], yaml_content)
